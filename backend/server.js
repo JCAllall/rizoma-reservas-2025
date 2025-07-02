2d5b02f8f0ed0a2de121072c48c64cb046cb7f7d
Juan Cruz Allall <allalljuancruz@gmail.com>
2025-07-02 05:11:42 -0300

    Corrijo server.js y registro

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") }); // ✅ usa ruta absoluta
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const express = require("express");
+const cors = require("cors"); // <-- AÑADIR ESTA LÍNEA
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors()); // <- habilita acceso desde el frontend
-app.use(express.json());
-
+app.use(cors());
 // ✅ importá correctamente tu archivo
 const reservasRouter = require("./routes/reservationRoutes");
 
